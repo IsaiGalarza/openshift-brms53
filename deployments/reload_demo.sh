6a3129b0982e11c14bd80a9230a9bd7e9d019b69
Eric D. Schabell <eric@schabell.org>
2012-08-20 15:26:06 -0400

    Fixed problems with paths in bash.

--- deployments/reload_demo.sh
+++ deployments/reload_demo.sh
@@ -18,19 +18,19 @@ cd $OPENSHIFT_HOMEDIR
 
 echo "Checking if artifacts are already deployed."
 echo
-if [ -e $deploy/designer.war.deployed ]; then
+if [ -e "{$deploy}/designer.war.deployed" ]; then
 	echo "Found deployed designer.war, undeploying this artifact."
   echo
-	rm $deploy/designer.war.deployed
+	rm "{$deploy}/designer.war.deployed"
 	sleep 5s
 	echo "Finished with undeploy!"
 	echo
 fi
 
-if [ -e $deploy/jboss-brms.war.deployed ]; then
+if [ -e "{$deploy}/jboss-brms.war.deployed" ]; then
 	echo "Found deployed jboss-brms.war, undeploying this artifact."
   echo
-	rm $deploy/jboss-brms.war.deployed
+	rm "{$deploy}/jboss-brms.war.deployed"
 	sleep 4s
 	echo "Waiting for undeploy to finish..."
 	sleep 4s
@@ -41,14 +41,14 @@ if [ -e $deploy/jboss-brms.war.deployed ]; then
 fi
 
 echo "Setup for delay of BRMS Designer deployment."
-touch $deploy/designer.war.dodeploy.delayed
+touch "{$deploy}/designer.war.dodeploy.delayed"
 echo
 
 echo "In a few minutes the BRMS BRM will be available."
-if [ -e $deploy/jboss-brms.war.undeployed ]; then
-	mv $deploy/jboss-brms.war.undeployed $deploy/jboss-brms.war.dodeploy
+if [ -e "{$deploy}/jboss-brms.war.undeployed" ]; then
+	mv "{$deploy}/jboss-brms.war.undeployed" "{$deploy}/jboss-brms.war.dodeploy"
 else
-	touch $deploy/jboss-brms.war.dodeploy
+	touch "{$deploy}/jboss-brms.war.dodeploy"
 fi
 echo
 
@@ -77,7 +77,7 @@ echo
 sleep 3s
 
 # start designer deployment.
-mv $deploy/designer.war.dodeploy.delayed $deploy/designer.war.dodeploy
+mv "{$deploy}/designer.war.dodeploy.delayed" "{$deploy}/designer.war.dodeploy"
 
 echo "In a few minutes the BRMS Designer will be available."
 echo
